ca2ad2141dcbbc8b554c679c46f9e18db8bc8a69
seanyboyswag <2150218@slu.edu.ph>
2020-02-09 10:47:01 +0800

    Playground: Array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -15,14 +15,23 @@
 //     console.log(`It's ${temp} in ${city}`);   
 // }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const { name: publisherName = 'Self-Published' } =  book.publisher
+// const { name: publisherName = 'Self-Published' } =  book.publisher
 
-console.log(publisherName); // Penguin, default of Self-Published
+// console.log(publisherName); // Penguin, default of Self-Published
+
+// const address = ['124 M. Roxas Street', 'Lower Brookside', 'Baguio', '2600'];
+// const [ , , city, zip ] = address;
+// console.log(`You are in  ${city} ${zip}` );
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$69.75'];
+const [ itemName, , , itemPrice ] = item;
+
+console.log(`A medium ${itemName} costs ${itemPrice}`);
