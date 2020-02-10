cd4471b2ef0616a01964ad53b986af07872e8d70
seanyboyswag <2150218@slu.edu.ph>
2020-02-10 23:34:18 +0800

    Playground: Filtering Redux data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -38,10 +38,28 @@ const setTextFilter = (text = '') => ({
     type: 'SET_TEXT_FILTER',
     text
 });
+
 // SORT_BY_DATE
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+});
+
 // SORT_BY_AMOUNT
+const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+});
+
 // SET_START_DATE
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
 // SET_END_DATE
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
 
 // Expenses Reducer
 const expensesReducer = (state = [], action) => {
@@ -78,39 +96,83 @@ const filtersReducerDefaultState = {
 
 // Filters reducer
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
-        switch (action.type) {
-            case 'SET_TEXT_FILTER':
-                return {
-                    ...state,
-                    text: action.text
-                }
-            default:
-                return state;
-        }
+    switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            }
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            }
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            }
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            }
+        case 'SET_END_DATE':
+        return {
+                ...state,
+                endDate: action.endDate
+            }
+        default:
+            return state;
+    }
 
+};
+
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text = '', sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+
+
+        return startDateMatch && endDateMatch && textMatch;
+    });
 };
 
 // Store creation
 const store = createStore(
     combineReducers({
         expenses: expensesReducer,
-        filter: filtersReducer
+        filters: filtersReducer
     })
 );
 
 store.subscribe(() => {
-    console.log(store.getState())
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+    console.log(visibleExpenses)
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 100 }))
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 , createdAt: 1000}))
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 100, createdAt: -1000 }))
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
-store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter('rent'));
-store.dispatch(setTextFilter());
+// store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter());
+
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+// store.dispatch(setStartDate(0));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(1000));
+// store.dispatch(setEndDate());
+store.dispatch(setTextFilter('ent'));
 
 const demoState = {
     expenses: [{
@@ -127,12 +189,3 @@ const demoState = {
         endDate: undefined
     }
 }
-
-const user = {
-    name: 'Sean',
-    age: 22
-};
-
-console.log({
-    ...user
-});
