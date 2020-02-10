4b7f03451c1d23c07fe8dc3697a0e2bd7b3de091
seanyboyswag <2150218@slu.edu.ph>
2020-02-10 22:34:36 +0800

    Playground: Object dpread operator

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -27,7 +27,17 @@ const removeExpense = ({ id }) => ({
 })
 
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -42,7 +52,18 @@ const expensesReducer = (state = [], action) => {
                 action.expense
             ]
         case 'REMOVE_EXPENSE':
-            return state.filter(({ id }) => (id !== action.id))
+            return state.filter(({ id }) => (id !== action.id));
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                }
+            });
         default:
             return state;
     }
@@ -58,6 +79,11 @@ const filtersReducerDefaultState = {
 // Filters reducer
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
         switch (action.type) {
+            case 'SET_TEXT_FILTER':
+                return {
+                    ...state,
+                    text: action.text
+                }
             default:
                 return state;
         }
@@ -81,6 +107,11 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 10
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
+
 const demoState = {
     expenses: [{
         id: 'asdasdasd',
@@ -96,3 +127,12 @@ const demoState = {
         endDate: undefined
     }
 }
+
+const user = {
+    name: 'Sean',
+    age: 22
+};
+
+console.log({
+    ...user
+});
