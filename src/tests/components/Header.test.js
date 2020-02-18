86d51ad5712b5b8c2feb80e0141749a65117b610
seanyboyswag <seanygenove@gmail.com>
2020-02-18 14:11:14 +0800

    Tests: Added snapshot testing with dynamic components

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme'
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from './../../components/Header';
 
 test('should render Header correctly', () => {
     const wrapper = shallow(<Header />);
     expect(wrapper).toMatchSnapshot();
-
-    // const renderer = new ReactShallowRenderer();
-    // renderer.render(<Header />);
-    // expect(renderer.getRenderOutput()).toMatchSnapshot();
-
-    // console.log(renderer.getRenderOutput());
 });
