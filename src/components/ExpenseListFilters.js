16130f1f7bd84548afe8e711dea74c78189a9a3c
seanyboyswag <2150218@slu.edu.ph>
2020-02-21 11:31:28 +0800

    Tests: Added tests for ExpenseListFilters component

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -3,67 +3,75 @@ import { connect } from 'react-redux';
 import { DateRangePicker } from 'react-dates';
 import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './../actions/filters'
 
-class ExpenseListFilters extends React.Component {
+export class ExpenseListFilters extends React.Component {
     state = {
         calendarFocused: null
     }
 
     onDatesChange = ({ startDate, endDate }) => {
-        this.props.dispatch(setStartDate(startDate));
-        this.props.dispatch(setEndDate(endDate));
+        this.props.setStartDate(startDate);
+        this.props.setEndDate(endDate);
     }
 
     onFocusChange = (calendarFocused) => {
         this.setState(() => ({ calendarFocused }));
     }
 
-    render() {
-        return (
-            <div>
-                <input
-                    type="text"
-                    value={this.props.filters.text}
-                    onChange={(e) => {
-                        this.props.dispatch(setTextFilter(e.target.value))
-                        console.log(e.target.value);
-                    }}
-                />
-                <select
-                    onChange={(e) => {
-                        if (e.target.value === 'date') {
-                            this.props.dispatch(sortByDate())
-                        } else if (e.target.value === 'amount') {
-                            this.props.dispatch(sortByAmount())
-                        }
-                        console.log(e.target.value)
-                    }}
-                >
-                    <option value="date">
-                        Date
-                    </option>
-                    <option value="amount">
-                        Amount
-                    </option>
-                </select>
-                <DateRangePicker
-                    startDate={this.props.filters.startDate}
-                    endDate={this.props.filters.endDate}
-                    onDatesChange={this.onDatesChange}
-                    focusedInput={this.state.calendarFocused}
-                    onFocusChange={this.onFocusChange}
-                    showClearDates={true}
-                    numberOfMonths={1}
-                    isOutsideRange={() => false}
-                />
-            </div>
-        );
+    onTextChange = (e) => {
+        this.props.setTextFilter(e.target.value);
+        console.log(e.target.value);
     }
+
+    onSortChange = (e) => {
+    if (e.target.value === 'date') {
+        this.props.sortByDate();
+    } else if (e.target.value === 'amount') {
+        this.props.sortByAmount();
+    }
+    console.log(e.target.value)
+}
+
+render() {
+    return (
+        <div>
+            <input
+                type="text"
+                value={this.props.filters.text}
+                onChange={this.onTextChange}
+            />
+            <select
+                onChange={this.onSortChange}
+            >
+                <option value="date">
+                    Date
+                    </option>
+                <option value="amount">
+                    Amount
+                    </option>
+            </select>
+            <DateRangePicker
+                startDate={this.props.filters.startDate}
+                endDate={this.props.filters.endDate}
+                onDatesChange={this.onDatesChange}
+                focusedInput={this.state.calendarFocused}
+                onFocusChange={this.onFocusChange}
+                showClearDates={true}
+                numberOfMonths={1}
+                isOutsideRange={() => false}
+            />
+        </div>
+    );
+}
 };
 
-const mapStateToProps = (state) => {
-    return {
-        filters: state.filters
-    }
-}
+const mapStateToProps = (state) => ({ filters: state.filters });
+
+const mapDispatchToProps = (dispatch) => ({
+    setTextFilter: (text) => dispatch(setTextFilter(text)),
+    sortByDate: () => dispatch(sortByDate()),
+    sortByAmount: () => dispatch(sortByAmount()),
+    setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+    setEndDate: (endDate) => dispatch(setEndDate(endDate))
+}) 
 
 export default connect(mapStateToProps)(ExpenseListFilters);
---
+++ src/tests/components/ExpenseListFilters.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpenseListFilters } from '../../components/ExpenseListFilters';
+import { filters, altFilters } from '../fixtures/filters.js';
+
+let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
+
+beforeEach(() => {
+    setTextFilter = jest.fn();
+    sortByDate = jest.fn();
+    sortByAmount = jest.fn();
+    setStartDate = jest.fn();
+    setEndDate = jest.fn();
+
+    wrapper = shallow(
+        <ExpenseListFilters
+            filters={filters}
+            setTextFilter={setTextFilter}
+            sortByDate={sortByDate}
+            sortByAmount={sortByAmount}
+            setStartDate={setStartDate}
+            setEndDate={setEndDate}
+        />);
+});
+
+test('Should render ExpenseListFilters correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should render ExpenseListFilters with altFilters correctly', () => {
+    wrapper.setProps({ filters: altFilters });
+    expect(wrapper).toMatchSnapshot();
+});
