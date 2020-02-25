4e203f1f658d220e6889aa1909c68bc0b2f2fc8a
seanyboyswag <2150218@slu.edu.ph>
2020-02-25 16:09:24 +0800

    Setup formatting for amoutn and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+
+// load a locale
+numeral.register('locale', 'php', {
+    delimiters: {
+        thousands: ',',
+        decimal: '.'
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal : function (number) {
+        return number === 1 ? 'er' : 'ème';
+    },
+    currency: {
+        symbol: '₱'
+    }
+});
+
+numeral.locale('php');
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount).format('$0,0.00')}
+            -
+            {moment(createdAt).format('MMM Do, YYYY')}u
+        </p>
     </div>
 );
 
