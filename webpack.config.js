77b6f617d9892b244a0a08cf7a86dc2289c95b90
seanyboyswag <2150218@slu.edu.ph>
2020-03-03 22:58:42 +0800

    Removed semi-colon typo in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-process.env.NODE_ENV = process.env.NODE_ENV; || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test' });
