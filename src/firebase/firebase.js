454ebbe3b11faf509e752b36ba08bb8ac2e28337
seanyboyswag <2150218@slu.edu.ph>
2020-03-03 22:47:59 +0800

    Setup integration with test database and for different environments

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAypFBR8ZNq9D9umI9jKfqXgWb3ak51Phg",
-    authDomain: "expensify-77c71.firebaseapp.com",
-    databaseURL: "https://expensify-77c71.firebaseio.com",
-    projectId: "expensify-77c71",
-    storageBucket: "expensify-77c71.appspot.com",
-    messagingSenderId: "500233728350",
-    appId: "1:500233728350:web:0f11d0554314ce1db91fcf",
-    measurementId: "G-MVS53N6TLV"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,5 +1,8 @@
 import Enzyme from 'enzyme';
 import Adapater from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
+
+DotEnv.config({ path: '.env.test' })
 
 Enzyme.configure({
     adapter: new Adapater()
