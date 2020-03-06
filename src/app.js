288e0abd398f2647b8edbfc5baf4d37a1b4759e0
seanyboyswag <seanygenove@gmail.com>
2020-03-06 14:42:17 +0800

    Initial redirect for login and logout

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -14,27 +12,33 @@ import 'react-dates/lib/css/_datepicker.css'
 import  { firebase } from './firebase/firebase';
 
 const store = configureStore();
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
 );
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {        
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+}
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
-});
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        console.log('log in')
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+
+            if (history.location.pathname === '/') {
+                history.push('/dashboard')
+            }
+        });
     } else {
-        console.log('log out')
+        renderApp();
+        history.push('/');
     }
 });
